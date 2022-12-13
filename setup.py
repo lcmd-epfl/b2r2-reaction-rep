4a36dc4900889b3c20ce6d00c46b0a3451a63c37
puck <puck.vangerwen@gmail.com>
2022-12-13 17:10:53 +0100

    trying with setuptools

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from setuptools import setup 
+
 __author__ = "Puck van Gerwen"
 __copyright__ = "Copyright 2022"
 __credits__ = ["Puck van Gerwen et al, Mach. Learn. Sci. Tech.:2022(3), 045005"]
@@ -6,6 +8,7 @@ __version__ = "0"
 __maintainer__ = "Puck van Gerwen"
 __email__ = "puck.vangerwen@epfl.ch"
 __description__ = "B2R2 reaction representations"
+__url__ = "https://github.com/lcmd-epfl/b2r2-reaction-rep"
 
 def requirements():
     with open('requirements.txt') as f:
@@ -13,7 +16,7 @@ def requirements():
 
 # use README.md as long description
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 def setup_reps():
@@ -31,12 +34,12 @@ def setup_reps():
         version=__version__,
         author=__author__,
         author_email=__email__,
+        url=__url__,
         platforms = 'Any',
         description = __description__,
         long_description = readme(),
         keywords = ['Machine Learning', 'Quantum Chemistry'],
         classifiers = [],
-        url = __url__,
         install_requires = requirements(),
 )
 
